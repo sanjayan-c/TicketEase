cea1cc415a1672671007a8787f372714a20c4f06
Sanjayan0 <sanjayan201@gmail.com>
2023-10-04 20:36:21 +0530

    Wallet Transactions Interface

--- app/src/main/java/com/example/ticketease/CustomerVirtualWallet.kt
+++ app/src/main/java/com/example/ticketease/CustomerVirtualWallet.kt
@@ -8,6 +8,7 @@ import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.ticketease.fragment.CustomerPendingTransactionsFragment
 import com.example.ticketease.fragment.CustomerTopUpFragment
 import com.example.ticketease.fragment.CustomerTransactionsFragment
 
@@ -16,8 +17,10 @@ class CustomerVirtualWallet : AppCompatActivity() {
     private lateinit var cusVirtualWalletBack : ImageView
     private lateinit var cusVirtualWalletTransactions : LinearLayout
     private lateinit var cusVirtualWalletTopUp : LinearLayout
+    private lateinit var cusVirtualPendingTransactions : LinearLayout
     private lateinit var cusVirtualWalletTransactionsImage : ImageView
     private lateinit var cusVirtualWalletTopUpImage : ImageView
+    private lateinit var cusVirtualPendingTransactionsImage : ImageView
 
     private var currentFragment: Fragment? = null
 
@@ -34,12 +37,14 @@ class CustomerVirtualWallet : AppCompatActivity() {
 
         cusVirtualWalletTransactions = findViewById(R.id.cusVirtualWalletTransactions)
         cusVirtualWalletTopUp = findViewById(R.id.cusVirtualWalletTopUp)
+        cusVirtualPendingTransactions = findViewById(R.id.cusVirtualPendingTransactions)
         cusVirtualWalletTransactionsImage = findViewById(R.id.cusVirtualWalletTransactionsImage)
         cusVirtualWalletTopUpImage = findViewById(R.id.cusVirtualWalletTopUpImage)
+        cusVirtualPendingTransactionsImage = findViewById(R.id.cusVirtualPendingTransactionsImage)
         // Initialize your fragments (replace with your actual fragments)
         val transactionsFragment = CustomerTransactionsFragment()
         val topUpFragment = CustomerTopUpFragment()
-
+        val pendingsFragment = CustomerPendingTransactionsFragment()
         // Set the initial fragment (e.g., Transactions)
         setFragment(transactionsFragment)
 
@@ -48,12 +53,21 @@ class CustomerVirtualWallet : AppCompatActivity() {
             setFragment(transactionsFragment)
             cusVirtualWalletTransactionsImage.setImageResource(R.drawable.cus_my_wallet_transactions_selected)
             cusVirtualWalletTopUpImage.setImageResource(R.drawable.cus_my_wallet_topup)
+            cusVirtualPendingTransactionsImage.setImageResource(R.drawable.cus_my_wallet_pendings)
         }
 
         cusVirtualWalletTopUp.setOnClickListener {
             setFragment(topUpFragment)
             cusVirtualWalletTransactionsImage.setImageResource(R.drawable.cus_my_wallet_transactions)
             cusVirtualWalletTopUpImage.setImageResource(R.drawable.cus_my_wallet_topup_selected)
+            cusVirtualPendingTransactionsImage.setImageResource(R.drawable.cus_my_wallet_pendings)
+        }
+
+        cusVirtualPendingTransactions.setOnClickListener {
+            setFragment(pendingsFragment)
+            cusVirtualWalletTransactionsImage.setImageResource(R.drawable.cus_my_wallet_transactions)
+            cusVirtualWalletTopUpImage.setImageResource(R.drawable.cus_my_wallet_topup)
+            cusVirtualPendingTransactionsImage.setImageResource(R.drawable.cus_my_wallet_pendings_selected)
         }
     }
 
